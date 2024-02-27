9a6d794c7f7b98ddb61fe34c546a0c46994e2e7e
Krishna00008 <156284133+Krishna00008@users.noreply.github.com>
2024-02-27 15:19:45 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,4 +1,4 @@
-api_id = 26435700
-api_hash = "527cf5174e120a9093611bc69d7b7709"
-bot_token = "6180767554:AAGKw-Eo6L0DXlL4BsrDmKItUewgdKGAtGo"
-AUTH_USERS = "5033114900"
+api_id = 28984365
+api_hash = "d75fed33855a0b556d040ef22f23eb8f"
+bot_token = "7156450292:AAHhfEwUXnVNjMxvXyAcPQV_d59ro3LvSI0"
+AUTH_USERS = "5829511291"
